9354cc2f9a67db7d5fc8d9376addea8720179f55
Akruti-Purohit <159265800+Akruti-Purohit@users.noreply.github.com>
2024-02-24 20:29:00 +0530

    Update P_10.java

--- P_10.java
+++ P_10.java
@@ -9,13 +9,38 @@ The class should have following methods:
 
 import java.util.Scanner;
 
+class Person{
+    String name;
+    int age;
+    String adres;
+
+    Person(String name,int age,String adres){
+        this.name = name;
+        this.age = age;
+        this.adres = adres;
+    }
+
+    void introduce(){
+        System.out.println("Your name is:"+name);
+        System.out.println("Your age is"+age);
+        System.out.println("Your address is:"+adres);
+    }
+}
 public class P_10 {
         public static void main(String[] args) {
         System.out.println("220130318033");
         System.out.println("Purohit Akruti K.");
 
         Scanner sc = new Scanner(System.in);
+            System.out.println("Enter your name:");
+            String name = sc.nextLine();
+            System.out.println("Enter your address:");
+            String adres = sc.nextLine();
+            System.out.println("Enter your age:");
+            int age = sc.nextInt();
 
+            Person p1 = new Person(name, age, adres);
+            p1.introduce();
         sc.close();
     }
 }
