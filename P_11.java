6cf4e58d7d82376ce47da962177a39c274458d23
Akruti-Purohit <159265800+Akruti-Purohit@users.noreply.github.com>
2024-02-24 20:29:37 +0530

    Update P_11.java

--- P_11.java
+++ P_11.java
@@ -5,13 +5,37 @@ message. */
 
 import java.util.Scanner;
 
+class Person2{
+    String name;
+    int age;
+    String adres;
+
+    void getter(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        name = sc.nextLine();
+        System.out.println("Enter your address:");
+        adres = sc.nextLine();
+        System.out.println("Enter your age:");
+        age = sc.nextInt();
+
+        sc.close();
+    }
+
+    void introduce(){
+        System.out.println("Your name is:"+name);
+        System.out.println("Your age is:"+age);
+        System.out.println("Your address is:"+adres);
+    }
+}
 public class P_11 {
         public static void main(String[] args) {
         System.out.println("220130318033");
         System.out.println("Purohit Akruti K.");
 
-        Scanner sc = new Scanner(System.in);
+        Person2 p2 = new Person2();
+        p2.getter();
+        p2.introduce();
 
-        sc.close();
     }
 }
